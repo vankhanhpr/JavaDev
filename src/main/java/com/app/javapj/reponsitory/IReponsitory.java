6be50aaf281f8ae3d921664787ce6d0b7864f747
vankhanhpr <donkihote2305@gmail.com>
2023-12-11 16:28:37 +0700

    fix

--- src/main/java/com/app/javapj/reponsitory/IReponsitory.java
+++ src/main/java/com/app/javapj/reponsitory/IReponsitory.java
@@ -5,5 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.io.Serializable;
 import java.util.List;
 
-public interface IReponsitory<T ,ID extends Serializable>  extends JpaRepository<T, ID> {
+public interface IReponsitory<T> {
+    void add(T entity);
+    void delete(String id);
+    void update(T entity);
+    T get(String id);
+    boolean exists(String id);
 }
--- src/main/java/com/app/javapj/reponsitory/impl/Reponsitory.java
+++ src/main/java/com/app/javapj/reponsitory/impl/Reponsitory.java
@@ -4,49 +4,36 @@ import com.app.javapj.reponsitory.IReponsitory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.io.Serializable;
 import java.util.List;
 
-public class Reponsitory<T, ID extends Serializable> implements IReponsitory<T, ID> {
+public class Reponsitory<T> implements IReponsitory<T> {
+
 
     @Override
-    public List<T> findAll() {
+    public void add(T entity) {
+
+    }
+
+    @Override
+    public void delete(String id) {
+
+    }
+
+    @Override
+    public void update(T entity) {
+
+    }
+
+    @Override
+    public T get(String id) {
         return null;
     }
 
     @Override
-    public List<T> findAll(Sort sort) {
-        return null;
-    }
-
-    @Override
-    public Page<T> findAll(Pageable pageable) {
-        return null;
-    }
-
-    @Override
-    public <S extends T> List<S> save(Iterable<S> iterable) {
-        return null;
-    }
-
-    @Override
-    public void flush() {
-
-    }
-
-    @Override
-    public T saveAndFlush(T t) {
-        return null;
-    }
-
-    @Override
-    public void deleteInBatch(Iterable<T> iterable) {
-
-    }
-
-    @Override
-    public void deleteAllInBatch() {
-
+    public boolean exists(String id) {
+        return false;
     }
 }
